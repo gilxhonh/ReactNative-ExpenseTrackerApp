718a36c67825e875d1b522b14ceb872599e525ab
Gilxhon001 <gilxhonhima@gmail.com>
2023-07-12 14:56:48 +0200

    add ui elements and context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,44 +4,11 @@ import ExpensesSummary from './ExpensesSummary'
 import ExpensesList from './ExpensesList'
 import { GlobalStyles } from '../../constants/styles'
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-05-16')
-  },
-  {
-    id: 'e2',
-    description: 'A pair of shoes',
-    amount: 89.99,
-    date: new Date('2023-05-15')
-  },
-  {
-    id: 'e3',
-    description: 'A pair of shoes',
-    amount: 159.99,
-    date: new Date('2023-05-14')
-  },
-  {
-    id: 'e4',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-04-10')
-  },
-  {
-    id: 'e5',
-    description: 'Shoes',
-    amount: 1059.99,
-    date: new Date('2023-01-05')
-  }
-]
-
 function ExpensesOutput ({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary periodName={expensesPeriod} expenses={DUMMY_EXPENSES} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary periodName={expensesPeriod} expenses={expenses} />
+      <ExpensesList expenses={expenses} />
     </View>
   )
 }
